e7377ded943e5eccca00a00e7050d7a45056f0d6
Adam Thomson <adam.thomson.smc@gmail.com>
2016-04-24 22:09:32 -0600

    Start adding file descriptions

---
+++ nf_generate_network.py
@@ -0,0 +1,5 @@
+'''
+Here, the network is randomly constructed.  Each variable controls the min and max number of nodes of each type, they
+default to the values specified in config.txt but can be manually specified at the console.  The graph will be output
+as a pair of .csv's in the format nodes.csv:"#node,demand(-)/offer(+)" - arcs.csv:"#arc,#tail,#head,capacity,cost"
+'''
---
+++ nf_sp.py
@@ -0,0 +1,4 @@
+'''
+Using Djikstra's algorithm, find the shortest path to every node from the source node.  Write the solution as a .csv
+in the format:"#node,#parent".
+'''
